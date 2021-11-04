8bb253d855a496c4ef0a29e88c20f96095ce2ff6
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-11-04 17:53:53 -0300

    Add animation on hover

--- components/IconLink.js
+++ components/IconLink.js
@@ -4,7 +4,7 @@ export default function IconLink({ children, href, target = '_blank', title }) {
       href={href}
       target={target}
       title={title}
-      className="bg-indigo-500 hover:opacity-95 text-white p-2 rounded-full cursor-pointer flex h-9 w-9 items-center justify-center"
+      className="bg-indigo-600 hover:opacity-95 text-white p-2 rounded-full cursor-pointer flex h-9 w-9 items-center justify-center hover:scale-105 transition"
     >
       {children}
     </a>
