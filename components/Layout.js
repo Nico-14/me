96adcc8c66e44a70ab5e5d1a4b5e2abbd39f6493
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-11-04 00:41:29 -0300

    Add nav menu

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const navLinks = [
   { href: '/', text: 'Inicio' },
@@ -11,21 +12,45 @@ const navLinks = [
 
 export default function Layout({ children }) {
   const { pathname } = useRouter();
+  const [isMenuShowing, setIsMenuShowing] = useState();
+
+  const handleMenuClick = () => {
+    setIsMenuShowing(!isMenuShowing);
+  };
   return (
     <div className="flex flex-col min-h-screen">
       <Head>
         <title>Mateo Ledesma</title>
       </Head>
-      <header className="flex w-full justify-center px-4 md:px-14 mt-4">
-        <nav className="flex gap-x-5 items-center">
+      <header className="flex w-full justify-center px-4 md:px-14 mt-4 relative">
+        <button className="sm:hidden ml-auto" onClick={handleMenuClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-9 w-9 text-indigo-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+        <nav
+          className={`gap-x-5 ${
+            isMenuShowing ? 'absolute flex' : 'hidden'
+          } bg-white flex-col w-full pb-4 px-6 z-10 top-full sm:flex sm:static sm:flex-row sm:items-center sm:w-auto sm:p-0 drop-shadow-2xl sm:drop-shadow-none`}
+        >
           {navLinks.map(({ href, text }) => (
             <Link href={href} key={href}>
-              <a className={`font-medium hover:text-gray-600 ${pathname === href ? 'text-gray-600' : 'text-gray-500'}`}>
+              <a
+                className={`font-medium hover:text-gray-600 ${
+                  pathname === href ? 'text-gray-700' : 'text-gray-500'
+                } py-2 sm:py-0`}
+              >
                 {text}
               </a>
             </Link>
           ))}
-          <button className="bg-indigo-600 hover:opacity-95 text-white rounded px-4 py-2">
+          <button className="bg-indigo-600 hover:opacity-95 text-white rounded px-4 py-2 mt-2 sm:mt-0">
             <a href="/cv.pdf" download="CV Mateo Ledesma.pdf">
               Descarga CV
             </a>
