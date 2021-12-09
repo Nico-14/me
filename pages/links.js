33bbd8e80016504bc495c7c36ca7aa137afcfc04
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-09 13:58:20 -0300

    Change links page buttons text color

--- pages/links.js
+++ pages/links.js
@@ -4,7 +4,7 @@ import data from '../data.json';
 
 export default function Links() {
   const [origin, setOrigin] = useState();
-  
+
   useEffect(() => {
     setOrigin(window.location.origin);
   }, []);
@@ -16,7 +16,7 @@ export default function Links() {
           {data.links.map(({ url, text }) => (
             <li
               key={url}
-              className="rounded hover:opacity-95 cursor-pointer my-6 font-medium border-2 border-pink-600 text-pink-500 text-md text-center hover:bg-pink-600 hover:text-black"
+              className="rounded hover:opacity-95 cursor-pointer my-6 font-medium border-2 border-pink-600 text-white text-md text-center hover:bg-pink-600"
             >
               <a href={url.replace('%HOST%', origin)} target="_blank" className="block py-3">
                 {text}
