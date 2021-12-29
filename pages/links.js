9c7437d8715015de35be3d3d0658528c6b6f19b5
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-29 14:32:05 -0300

    Add portoflio link, remove flex center

--- pages/links.js
+++ pages/links.js
@@ -11,20 +11,18 @@ export default function Links() {
 
   return (
     <Layout>
-      <nav className="w-full max-w-3xl">
-        <ul>
-          {data.links.map(({ url, text }) => (
-            <li
-              key={url}
-              className="hover:opacity-95 cursor-pointer my-6 font-medium border-2 border-pink-600 text-white text-md text-center hover:bg-pink-600"
-            >
-              <a href={url.replace('%HOST%', origin)} target="_blank" className="block py-3">
-                {text}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <ul className="w-full max-w-3xl my-auto">
+        {data.links.map(({ url, text }) => (
+          <li
+            key={url}
+            className="hover:opacity-95 cursor-pointer my-6 font-medium border-2 border-pink-600 text-white text-md text-center hover:bg-pink-600"
+          >
+            <a href={url.replace('%HOST%', origin)} target="_blank" className="block py-3">
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
