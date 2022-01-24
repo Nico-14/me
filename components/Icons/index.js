4ee5a205dbe63df1d8fca3087bd2f9e0f58e1dfd
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-01-23 22:45:50 -0300

    Extract icons

---
+++ components/Icons/index.js
@@ -0,0 +1,8 @@
+import Github from './Github';
+import Instagram from './Instagram';
+import Linkedin from './Linkedin';
+import Mail from './Mail';
+import Whatsapp from './Whatsapp';
+import ChevronDown from './ChevronDown';
+
+export { Github, Instagram, Linkedin, Mail, Whatsapp, ChevronDown };
