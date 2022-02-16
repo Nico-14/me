156737c9f145909a2a3602a9706d4ae4a79c7b72
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-16 19:40:04 -0300

    Update projects page heading

--- pages/projects.js
+++ pages/projects.js
@@ -51,7 +51,7 @@ export default function Projects({ data }) {
   return (
     <Layout>
       <section className="w-full">
-        <h1 className="text-4xl sm:text-5xl text-white font-medium">Portfolio</h1>
+        <h1 className="text-4xl sm:text-5xl text-white font-medium">Proyectos</h1>
         <div className="mt-8 grid gap-x-10 gap-y-12 auto-rows-min grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {data?.projects?.map((project) => (
             <Card project={project} key={project.name} />
