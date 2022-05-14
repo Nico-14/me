3f924c62b084781f2af97d1ed0be2a5c03e7e677
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-05-14 19:41:54 -0300

    Change project card style and links

--- components/ButtonLink.js
+++ components/ButtonLink.js
@@ -1,9 +1,10 @@
 import { forwardRef } from 'react';
 
-const ButtonLink = forwardRef(({ href, children, className }, ref) => (
+const ButtonLink = forwardRef(({ href, children, className,target }, ref) => (
   <a
     href={href}
     ref={ref}
+    target={target}
     className={`font-bold text-sm px-5 py-2 text-white rounded-xl bg-emerald-600 hover:opacity-90 transition-opacity  ${
       className ? className : ''
     }`}
--- components/Project.js
+++ components/Project.js
@@ -1,45 +1,53 @@
+import ButtonLink from './ButtonLink';
 import { Github } from './Icons';
+import LinkIcon from './Icons/LinkIcon';
 
 const Project = ({ project }) => (
-  <a href={project.link} target="_blank">
-    <article className="cursor-pointer rounded-lg overflow-hidden bg-zinc-900/40 h-full flex flex-col group">
-      <div className="aspect-[16/8] overflow-hidden flex mx-5 my-4">
+  <article className="rounded-lg overflow-hidden bg-zinc-900/40 h-full flex flex-col">
+    <a href={project.link} target="_blank">
+      <div className="aspect-[16/8] overflow-hidden flex mx-5 my-4 group">
         <img
           src={project.images[0]}
           className="object-cover w-full group-hover:scale-110 group-active:scale-110 transition rounded"
           alt={`${project.name} demo`}
         ></img>
       </div>
+    </a>
 
-      <div className="px-5 py-4 text-white flex flex-1 flex-col gap-6">
-        <div>
+    <div className="px-5 text-white flex flex-1 flex-col gap-6">
+      <div className="mb-2">
+        <a href={project.link} target="_blank">
           <header>
             <h2 className="text-2xl">{project.name}</h2>
           </header>
-          <div>
-            <p className="text-zinc-300 leading-5 whitespace-pre-line mt-2">
-              {project.description}
-            </p>
-            <button
-              className="bg-zinc-700 flex items-center px-3 py-2 gap-2 rounded-xl text-sm hover:opacity-70 transition-opacity font-bold mt-3"
-              onClick={() => window.open(project.repo, '_blank')}
-            >
-              <Github className="h-5 w-5 fill-white" />
-              Repositorio
-            </button>
-          </div>
-        </div>
-
-        <div className="mt-auto flex flex-wrap gap-2">
-          {project.tags?.map((tag) => (
-            <span className="bg-zinc-700 text-zinc-300 text-sm rounded-xl px-2" key={tag}>
-              {tag}
-            </span>
-          ))}
+        </a>
+        <div>
+          <p className="text-zinc-300 leading-5 whitespace-pre-line mt-2">{project.description}</p>
         </div>
       </div>
-    </article>
-  </a>
+      <div className="flex flex-wrap gap-3 mt-auto">
+        <ButtonLink
+          className="bg-zinc-700 flex gap-2 px-3"
+          href={project.repo}
+          target="_blank"
+        >
+          <Github className="h-5 w-5 fill-white" />
+          Repositorio
+        </ButtonLink>
+        <ButtonLink className="flex gap-2 px-3" href={project.link} target="_blank">
+          <LinkIcon className="h-5 w-5 fill-white" />
+          Abrir
+        </ButtonLink>
+      </div>
+      <div className="flex flex-wrap gap-2 mb-4">
+        {project.tags?.map((tag) => (
+          <span className="bg-zinc-700 text-zinc-300 text-sm rounded-xl px-2" key={tag}>
+            {tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  </article>
 );
 
 export default Project;
