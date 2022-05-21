3990f0aec3d013e62b096ec72d8f64965abef7d0
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-05-21 19:15:25 -0300

    Update nav links

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,23 @@ import ButtonLink from './ButtonLink';
 import IconLink from './IconLink';
 import * as Icons from './Icons';
 
-export default function Layout({ children }) {
+function NavLink({ children, href }) {
   const { pathname } = useRouter();
+
+  return (
+    <Link href={href}>
+      <a
+        className={`font-bold hover:text-zinc-100 transition-colors ${
+          pathname === href ? 'text-white' : ''
+        } py-2 sm:py-0`}
+      >
+        Inicio
+      </a>
+    </Link>
+  );
+}
+
+export default function Layout({ children }) {
   const [isMenuShowing, setIsMenuShowing] = useState();
 
   const handleMenuClick = () => {
@@ -21,44 +36,9 @@ export default function Layout({ children }) {
             isMenuShowing ? 'flex absolute' : 'hidden'
           } text-zinc-400 bg-zinc-800 px-6 pb-4 w-full top-full left-0 flex-col sm:w-auto sm:static sm:flex sm:flex-row sm:items-center sm:py-3 sm:my-6 sm:rounded-xl transition`}
         >
-          <Link href="/">
-            <a
-              className={`font-bold hover:text-zinc-100 transition-colors ${
-                pathname === '/' ? 'text-white' : ''
-              } py-2 sm:py-0`}
-            >
-              Inicio
-            </a>
-          </Link>
-          <Link href="/projects">
-            <a
-              className={`font-bold hover:text-zinc-100 transition-colors ${
-                pathname === '/projects' ? 'text-white' : ''
-              } py-2 sm:py-0`}
-            >
-              Proyectos
-            </a>
-          </Link>
-
-          <Link href="/contact">
-            <a
-              className={`font-bold hover:text-zinc-100 transition-colors ${
-                pathname === '/contact' ? 'text-white' : ''
-              } py-2 sm:py-0`}
-            >
-              Contacto
-            </a>
-          </Link>
-          <Link href="/cv">
-            <a
-              className={`font-bold hover:text-zinc-100 transition-colors ${
-                pathname === '/cv' ? 'text-white' : ''
-              } py-2 sm:py-0`}
-              target="_blank"
-            >
-              CV Online
-            </a>
-          </Link>
+          <NavLink href="/">Inicio</NavLink>
+          <NavLink href="/projects">Proyectos</NavLink>
+          <NavLink href="/contact">Contacto</NavLink>
           <ButtonLink
             href="https://raw.githubusercontent.com/mateo-14/personal-web-data/main/cv.pdf"
             className="mt-2 sm:mt-0"
@@ -68,13 +48,17 @@ export default function Layout({ children }) {
         </nav>
 
         <button
-          className={`sm:hidden ml-auto mr-2 text-white ${isMenuShowing ? 'opacity-1' : 'opacity-75'}`}
+          className={`sm:hidden ml-auto mr-2 text-white ${
+            isMenuShowing ? 'opacity-1' : 'opacity-75'
+          }`}
           onClick={handleMenuClick}
         >
           <Icons.ChevronDown className="h-9 w-9" />
         </button>
       </header>
-      <main className="flex-1 px-6 md:px-20 flex justify-center container mx-auto mt-4 sm:mt-0">{children}</main>
+      <main className="flex-1 px-6 md:px-20 flex justify-center container mx-auto mt-4 sm:mt-0">
+        {children}
+      </main>
       <footer className="flex justify-center">
         <div className="flex mb-4 mt-6 gap-x-3 ">
           <IconLink href="https://www.linkedin.com/in/mateo-ledesma/" title="LinkedIn">
