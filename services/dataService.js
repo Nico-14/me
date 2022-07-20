6b06b3cabce31bf1d0e7ad24bf796e3e8515e14a
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-07-19 23:10:19 -0300

    Remove unused dataService methods

--- services/dataService.js
+++ services/dataService.js
@@ -3,13 +3,6 @@ import axios from 'axios';
 const axiosClient = axios.create({
   baseURL: 'https://raw.githubusercontent.com/mateo-14/personal-web-data/main',
 });
-export function getAboutData() {
-  return axiosClient.get('about/data.json').then((res) => res.data);
-}
-
-export function getLinks() {
-  return axiosClient.get('links/data.json').then((res) => res.data);
-}
 
 export function getProjects() {
   return axiosClient.get('portfolio/projects/data.json').then((res) => res.data);
