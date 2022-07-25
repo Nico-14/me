68bc34c165110f9fdf8a303ba344183c3fe6ab4f
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-07-24 23:28:10 -0300

    Remove meta description

--- pages/_document.js
+++ pages/_document.js
@@ -10,27 +10,15 @@ export default function Document() {
           rel="stylesheet"
         />
         <meta name="title" content="Mateo Ledesma - Full Stack Developer" />
-        <meta
-          name="description"
-          content="I'm Mateo Ledesma and I'm JavaScript Full Stack Developer."
-        />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://mateoledesma.vercel.app/" />
         <meta property="og:title" content="Mateo Ledesma - Full Stack Developer" />
-        <meta
-          property="og:description"
-          content="I'm Mateo Ledesma and I'm JavaScript Full Stack Developer."
-        />
         <meta property="og:image" content="/banner.jpg" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://mateoledesma.vercel.app/" />
         <meta property="twitter:title" content="Mateo Ledesma - Full Stack Developer" />
-        <meta
-          property="twitter:description"
-          content="I'm Mateo Ledesma and I'm JavaScript Full Stack Developer."
-        />
         <meta property="twitter:image" content="/banner.jpg"></meta>
       </Head>
       <body>
