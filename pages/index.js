4905606624dac284ca051588091c64d27cb1c754
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-09-08 16:10:29 -0300

    update about

--- pages/index.js
+++ pages/index.js
@@ -30,12 +30,12 @@ export default function About({ data }) {
                 Full Stack Developer
               </h2>
               <p className="text-lg sm:text-xl lg:text-2xl mt-4 text-gray-300 whitespace-pre-line max-w-3xl text-left">
-                {`Hola 👋🏻! Me llamo Mateo y soy desarrollador Full Stack. En esta web podrás ver los distintos `}
-                <a href="#projects" className="underline hover:opacity-80 transition-opacity">proyectos</a> 👈🏻.
-                {` que he realizado y conocer más sobre mí.`} 
+                {'Hola 👋🏻! Me llamo Mateo y soy desarrollador Full Stack. En esta web podrás ver los distintos '}
+                <a href="#projects" className="underline hover:opacity-80 transition-opacity">proyectos</a>
+                {' que he realizado.'}
               </p>
               <p className="text-lg sm:text-xl lg:text-2xl text-gray-300 whitespace-pre-line max-w-3xl text-left">
-                Te invito a conocer más sobre mí en mi
+                {'Te invito a conocer más sobre mí en mi '}
                 <Link href="/cv">
                   <a className="underline hover:opacity-80 transition-opacity">currículum</a>
                 </Link> 📄.
